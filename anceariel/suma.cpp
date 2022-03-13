3f176dd9d60048664a9846fb507a64957deb59e7
artivol31 <ariel.ance.alarcon@utelvt.edu.ec>
2022-03-13 13:45:26 -0500

    creado desde mi celular

---
+++ anceariel/suma.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float A,B,C;
+	cout<<"ingrese los valores de A B:  ";
+	cin>>A>>B;
+	C=A+B;
+	cout<<"el resultado es  :";
+	cout<<C<<endl;
+	return 0;
+}
