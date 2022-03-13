d99a05e4b1eaffa44202fbd678180e14c5dbdeb8
PervisZurita <pervis.zurita.valencia@utelvt.edu.ec>
2022-03-12 22:29:53 -0500

    creado desde mi celular

---
+++ zuritapervis/suma.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{	
+	float A,B,C;
+	cout<<"ingrese los valores de A B" ;
+	cin>>A>>B;
+	C=A+B;
+	cout<<"el resultado es :";
+	cout<<C;
+	return 0;
+}
+
+
