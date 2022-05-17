f1dd43122630018c93f7e4c7ce43c509a9e93a2e
alinayi16 <alisson.quinonez.canchingre@utelvt.edu.ec>
2022-05-17 04:52:19 -0500

    actalizacion de la actividad C2

--- QuinonezCanchingre/ACTIVIDAD-B2/QuinonezAlisson-Compara.cpp
+++ QuinonezCanchingre/ACTIVIDAD-B2/QuinonezAlisson-Compara.cpp
@@ -1,15 +1,31 @@
+//================================================
+//==> Nombre del programa: comparacion de dos numeros
+//==> Archivo : compara.cpp
+//==>Autor: Alisson Quiñonez
+//==>Fecha de elaboración:2022-04-29 
+//==>Fecha ultima actualización:2022-05-17
+//=================================================
 #include<iostream>
 using namespace std;
 int main ()
 {
-	float a,b;
-	cout<<"ingrese los valores a b :"; cin>>a>>b;
-	if(a==b) {
+	float ac_a,ac_b;
+	cout<<"ingrese los valores a b :"; cin>>ac_a>>ac_b;
+	if(ac_a==ac_b) {
 		cout<<"son iguales:";
 	}else{
-		if(a>b) { cout<<"a es mayor";
-		}else{ cout<<"b es mayor";
+		if(ac_a>ac_b) { cout<<"El mayor es"<<ac_a;
+		}else{ cout<<" El mayor es"<<ac_b;
 		}
 	} 
+	cout<<endl<<"//================================================"<<endl;
+cout<<"//==> Nombre del programa:comparacion de dos mumeros"<<endl;
+cout<<"//==> Archivo : compara.cpp"<<endl;
+cout<<"//==>Autor: Alisson Quiñonez"<<endl;
+cout<<"//==>Fecha de elaboración: 2022-04-29"<<endl;
+cout<<"//==>Fecha ultima actualización: 2022-05-17"<<endl;
+cout<<"//================================================="<<endl;
+
+
 	return 0;
 }
